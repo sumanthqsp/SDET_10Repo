c51dd61487d8c09418f48d37969e757dfca73f32
Sushma <Sushma@LAPTOP-B3ITVU90>
2021-03-15 12:55:26 +0530

    First Commit

---
+++ ElfProject/src/test/java/createUser/CreateUserDataTest.java
@@ -0,0 +1,21 @@
+package createUser;
+
+import org.testng.annotations.Test;
+
+public class CreateUserDataTest {
+
+	@Test
+	public void createUser() {
+		System.out.println("executed create user");
+	}
+	
+	@Test
+	public void createUserWithMobileNo() {
+		System.out.println("executed create user with mobile no");
+	}
+	
+	@Test
+	public void deleteUser() {
+		System.out.println("executed deleteUser user");
+	}
+}
